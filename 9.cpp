b88d15064dc3102cabcba9d6221beb2319a918bf
theRunCom <49315766+theRunCom@users.noreply.github.com>
2022-11-22 08:28:33 +0800

    Create 9.cpp

---
+++ 9.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) return 0;
+        string s = to_string(x);
+        return s == string(s.rbegin(), s.rend());
+    }
+};
